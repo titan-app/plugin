a989d3f169eba1e3ce86b7aca054a9c27e283e36
zxdstyle <zxdstyle@foxmail.com>
2023-08-26 23:53:02 +0800

    init

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin } from 'vite';
-
-interface microWebPluginParams {
-    styleAppendTo?: string;
-    entry?: string;
-}
-declare function RemoteModule({ styleAppendTo, entry }: microWebPluginParams): Plugin;
-
-export { RemoteModule as default, microWebPluginParams };
