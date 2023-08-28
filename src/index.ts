bf553ba34b6c0b4f930f7c49dd2b35e7ab244959
zxdstyle <zxdstyle@foxmail.com>
2023-08-28 20:12:54 +0800

    ignore

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
-import { Plugin, transformWithEsbuild } from 'vite';
+import { Plugin } from 'vite';
 
 export interface RemoteModuleOption {
 	styleAppendTo?: string;
 	entry?: string;
 }
 
-const imagesRE = new RegExp(`\\.(png|webp|jpg|gif|jpeg|tiff|svg|bmp)($|\\?)`);
-
-export function RemoteModule({ styleAppendTo = 'parentNode', entry = 'packages/index.tsx' }: RemoteModuleOption): Plugin {
+export function RemoteModule({ entry = 'packages/index.tsx' }: RemoteModuleOption): Plugin {
 	return {
 		name: 'vite-plugin-remote-module',
 		config(config) {
 			return {
 				build: {
-					target: 'es2020',
 					cssCodeSplit: false,
 					rollupOptions: {
 						preserveEntrySignatures: 'allow-extension',
@@ -21,68 +18,12 @@ export function RemoteModule({ styleAppendTo = 'parentNode', entry = 'packages/i
 					},
 					...config.build,
 				},
+				resolve: {
+					...config.resolve,
+					react: 'https://esm.sh/react@18.2.0',
+					'react-dom': 'https://esm.sh/react-dom@18.2.0',
+				},
 			};
-		},
-		transform(code, id) {
-			// 修正图片资源使用绝对地址
-			if (imagesRE.test(id)) {
-				return {
-					code: code.replace(/(export\s+default)\s+(".+")/, `$1 new URL($2, import.meta['url']).href`),
-					map: null,
-				};
-			}
-			return undefined;
-		},
-		async generateBundle(options, bundle) {
-			let entry: string | undefined;
-			const cssChunks: string[] = [];
-			for (const chunkName of Object.keys(bundle)) {
-				if (chunkName.includes('main') && chunkName.endsWith('.js')) {
-					entry = chunkName;
-				}
-				if (chunkName.endsWith('.css')) {
-					cssChunks.push(`./${chunkName}`);
-				}
-			}
-			if (entry) {
-				const cssChunksStr = JSON.stringify(cssChunks);
-
-				// 修正 css 插入的位置
-				const result = await transformWithEsbuild(
-					`
-import defineApp from './${entry}?microAppEnv';
-
-function createLink(href) {
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = href;
-  link.id = href;
-  return link;
-}
-
-const styles = ${cssChunksStr}.map(css => createLink(new URL(css, import.meta['url'])));
-
-defineApp.styleInject = (parentNode) => {
-  styles.forEach((link) => {
-    if (!document.getElementById(link.id)) {
-      ${styleAppendTo}.append(link);
-    }
-  });
-};
-
-export default defineApp;
-        `,
-					'main.js',
-					{ minify: true },
-				);
-
-				// 导出微量体积入口文件，配合 hash 和主应用时间戳缓存处理
-				this.emitFile({
-					fileName: 'main.js',
-					type: 'asset',
-					source: result.code,
-				});
-			}
 		},
 	};
 }
